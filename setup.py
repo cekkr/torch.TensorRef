c1a3b2918893e2c82235b40fb23c7fe0329d0cd3
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-03-10 17:53:41 +0100

    change of module name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="torch.TensorRef",
+    name="torchTensorRef",
     version="0.1",
     packages=find_packages(),
     install_requires=[
