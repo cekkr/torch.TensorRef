c2f42781bd49c8e0e0bfeb70924588b1973b6eee
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-03-20 19:29:46 +0100

    trying to understand the number of args needed

--- torchTensorRef/inspection.py
+++ torchTensorRef/inspection.py
@@ -1,20 +1,55 @@
 import inspect
 import types
 
+def find_arg_count(func):
+    arg_count = 0
+    while True:
+        args = [None] * arg_count
+        try:
+            func(*args)
+        except TypeError as e:
+            se = str(e)
+            if 'missing' in se:
+                spl = se.split('missing ')
+                spl = spl[1].splut(' ')[0]
+                return arg_count + int(spl)
+
+            if "positional argument" in str(e) or "expected at most" in str(e):
+                #print(f"Function '{func.__name__}' expects {arg_count-1} arguments.")
+                break
+            elif "takes no arguments" in str(e):
+                #print(f"Function '{func.__name__}' expects 0 arguments.")
+                return 0
+        except Exception as e:
+            # Catch all other exceptions to prevent the loop from breaking due to unrelated errors
+            #print(f"An exception occurred: {e}")
+            se = str(e)
+            if "can't be None" not in se:
+                return arg_count
+
+        arg_count += 1
+
+        if arg_count > 10:
+            return -1
+    return arg_count
+
+
 def IsCompiledFunction(func):
     # Check if the function is a compiled (built-in) function
     if not isinstance(func, types.BuiltinFunctionType):
         return -1
     
-    # If it is a compiled function, analyze its signature
+    # Attempt to retrieve the function's signature
     try:
         sig = inspect.signature(func)
-        # Count the number of parameters that are not *args or **kwargs
-        params = [param for param in sig.parameters.values()
-                  if param.kind not in (param.VAR_POSITIONAL, param.VAR_KEYWORD)]
-        return len(params)
-    except ValueError:
-        # If inspect.signature() cannot process the function, return -1
-        # This can happen with some built-in functions
-        return -1
+    except ValueError as e:
+        # Handle the case where a signature cannot be found
+        nArgs = find_arg_count(func)
+        return nArgs
+
+    # Count the parameters, excluding *args and **kwargs
+    params = [param for param in sig.parameters.values()
+              if param.kind not in (param.VAR_POSITIONAL, param.VAR_KEYWORD)]
+    return len(params)
+
 
