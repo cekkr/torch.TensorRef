e77dd31720a397f937816b09f0f146d08b4853be
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-03-20 21:07:17 +0100

    buffer persistent

--- torchTensorRef/hook.py
+++ torchTensorRef/hook.py
@@ -29,7 +29,7 @@ class Hooks:
     def module_register_parameter(self, name, param):
         self._parameters[name] = param
 
-    def module_register_buffer(self, name, tensor, persistent):
+    def module_register_buffer(self, name, tensor, persistent=None):
         self._buffers[name] = tensor
         #todo: persistent
 
